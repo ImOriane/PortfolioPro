c475ea9137fc26ebcd4ab0a51026eccf65fa8b8e
ImOriane <oriane.foy@gmail.com>
2025-09-04 11:25:13 +0200

    up 1

---
+++ nav.php
@@ -0,0 +1,22 @@
+
+<nav class="right">
+    <ul>
+        <li>
+            <a href="projets.html"
+            ><span class="numerotation">1.</span> Projets</a
+            >
+        </li>
+        <li>
+            <a href="apropos.html"
+            ><span class="numerotation">2.</span> À propos</a
+            >
+        </li>
+        <li>
+            <a href="contact.html"
+            ><span class="numerotation">3.</span> Contact</a
+            >
+        </li>
+
+
+    </ul>
+</nav>
