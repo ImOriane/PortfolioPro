f033a5cd3c85289f12f742c035a96d8b5cd94e69
ImOriane <oriane.foy@gmail.com>
2025-10-20 13:02:03 +0200

    Portfolio's Base

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-function fadeBackground(pagePrecedente, pageActuel) {
-
+function urlPrevious(n, s) {
+    n = n || 'url_prev';
+    s = s || sessionStorage;
+    var p = s.getItem(n);
+    p = p ? p : '';
+    s.setItem(n, location.href);
+    return p;
+}
+function fadeBackground (pageActuel) {
+    pagePrecedente=urlPrevious();
     if (pagePrecedente.includes("index.html")) {
       if (pageActuel !== "index.html") {
           document.querySelector("#bg").classList.add("bgfadeOut");
       }
     } else {
-      if (pageActuel == "index.html") {
+      if (pageActuel === "index.html") {
           document.querySelector("#bg").classList.add("bgfadeIn");
       }
     }
