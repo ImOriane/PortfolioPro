c08015bae74551397fea1249ab76750b22d8dd75
ImOriane <oriane.foy@gmail.com>
2025-10-28 21:49:23 +0100

    Particle fix

---
+++ script/particle.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    particlesJS('particles-js', {
+        particles: {
+            number: { value: 80, density: { enable: true, value_area: 800 } },
+            color: { value: '#ffffff' },
+            shape: { type: 'circle' },
+            opacity: { value: 0.5 },
+            size: { value: 5, random: true },
+            line_linked: { enable: true, distance: 150, color: '#ffffff', opacity: 0.4, width: 1 },
+            move: { enable: true, speed: 6, direction: 'none', out_mode: 'out' }
+        },
+        interactivity: {
+            detect_on: 'canvas',
+            events: { onhover: { enable: true, mode: 'repulse' }, onclick: { enable: true, mode: 'push' } },
+            modes: { grab: { distance: 400, line_linked: { opacity: 1 } }, repulse: { distance: 200 } }
+        },
+        retina_detect: true
+    });
+});
